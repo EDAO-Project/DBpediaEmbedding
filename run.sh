48758137b4acd159a555cbab14ca8be5044dc123
root <root@a256-gc1-19.srv.aau.dk>
2021-12-04 16:54:17 +0100

    No need for result/ folder

--- run.sh
+++ run.sh
@@ -11,7 +11,6 @@ for file in files/*
 do
     echo 'Generating RDF embeddings for' $file
     result="${result_dir}${file:6:-4}"
-    mkdir result
     docker run --rm -v ${PWD}:/data ghcr.io/dwslab/jrdf2vec -graph $file -walkDirectory $result
     echo 'Done'
     echo
